c1bef94d2f29a098dc2c8ba3e1a3d7e431361737
Andreas <andreasgdp@gmail.com>
2020-09-07 18:30:36 +0200

    Done w. homework

--- Det første program - Andreas P/Andet program/Date.java
+++ Det første program - Andreas P/Andet program/Date.java
@@ -1,46 +1,58 @@
 public class Date {
 	public static void main(String[] args) {
-		Date date = new Date(20200907);
-		System.out.println(date.getMonth());
+		Date dateOb = new Date(20200907);
+		System.out.println(dateOb.getMonth(dateOb.getDate()));
 	}
 
 	private int date;
 	private String dateString;
 
-	public Date(int i) {
-
-	}
-
-	public void Dato(int aDate) {
-		date = aDate; // Format: YYYYMMDD
-		dateString = Integer.toString(date);
+	// Constructor
+	public Date(int date) {
+		this.date = date;
+		this.dateString = Integer.toString(date);
 	}
 
 	public int getDate() {
-		return date;
+		return this.date;
 	}
 
 	public int getYear() {
-		return date / 10000;
+		return this.date / 10000;
 	}
 
-	public int getMonth() {
-		String first = String.valueOf(dateString.charAt(4));
-		String second = String.valueOf(dateString.charAt(5));
+	public int getMonth(int date) {
+		String first = String.valueOf(this.dateString.charAt(4));
+		String second = String.valueOf(this.dateString.charAt(5));
 		String month = first + second;
 		int monthNumber = Integer.parseInt(month);
 
 		return monthNumber;
 	}
 
-	// public int getDay()
-	// {
-	// return
-	// }
+	public int getDay() {
+		String first = String.valueOf(this.dateString.charAt(6));
+		String second = String.valueOf(this.dateString.charAt(7));
+		String day = first + second;
+		int dayNumber = Integer.parseInt(day);
 
-	// public int getKvartal()
-	// {
-	// return
-	// }
+		return dayNumber;
+	}
+
+	public int getQuarter() {
+		int quarter = 0;
+
+		if (this.getMonth(this.date) <= 3) {
+			quarter = 1;
+		} else if (this.getMonth(this.date) <= 6) {
+			quarter = 2;
+		} else if (this.getMonth(this.date) <= 9) {
+			quarter = 3;
+		} else {
+			quarter = 4;
+		}
+
+		return quarter;
+	}
 
 }
