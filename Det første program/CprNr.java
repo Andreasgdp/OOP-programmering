7dadafbbfe58315e837b85c5e1d4de4a4e6e6dc7
Andreas <andreasgdp@gmail.com>
2020-10-02 11:34:23 +0200

    Shit be work

--- Det første program/CprNr.java
+++ Det første program/CprNr.java
@@ -1,5 +1,6 @@
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import java.util.*;
 import util.Identifier;
 import util.TypeTester;
 
@@ -9,10 +10,12 @@ public class CprNr {
 	private Identifier identifier;
 	private TypeTester tt;
 	private Date date;
+	private List<Integer> cprWeights;
 
 	public static void main(String[] args) {
 		CprNr cpr = new CprNr("0502201972");
 		System.out.println(cpr.getAge());
+		System.out.println(cpr.isValid());
 
 	}
 
@@ -22,6 +25,8 @@ public class CprNr {
 		this.intNumber = Integer.parseInt(this.number);
 		this.identifier = this.getIdentifier();
 		this.date = this.getDate();
+		this.cprWeights = new ArrayList<Integer>();
+		this.cprWeights.addAll(Arrays.asList(4, 3, 2, 7, 6, 5, 4, 3, 2, 1));
 	}
 
 	private Identifier getIdentifier() {
@@ -60,8 +65,7 @@ public class CprNr {
 	}
 
 	public int getYear() {
-		int date = this.intNumber / 10000;
-		String lastDigitsYear = String.format("%02d", (date / 10000) % 100);
+		String lastDigitsYear = String.format("%02d", (this.intNumber / 10000) % 100);
 		String firstDigitsYear = "00";
 		String stringYear;
 		int year;
@@ -82,10 +86,49 @@ public class CprNr {
 	public int getAge() {
 		Date currDate = this.getCurrDate();
 		int yearDiff = java.lang.Math.abs(currDate.getYear() - this.date.getYear() - 1);
-		if (this.date.getMonth() >= currDate.getMonth() && this.date.getDay() >= currDate.getDay())
+
+		if (this.date.getMonth() < currDate.getMonth()
+				|| (this.date.getMonth() == currDate.getMonth() && this.date.getDay() <= currDate.getDay()))
 			yearDiff++;
 
 		return yearDiff;
+	}
 
+	public boolean isMan() {
+		return (this.identifier.fourth % 2 == 1) ? true : false;
+	}
+
+	public boolean isWoman() {
+		return !this.isMan();
+	}
+
+	private boolean isNumeric(String strNum) {
+		if (strNum == null) {
+			return false;
+		}
+		try {
+			double d = Double.parseDouble(strNum);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
+	private int calcWeightCPR(String cprNr) {
+		int counter = 0;
+		for (int i = 0; i < cprNr.length(); i++) {
+			int num = Character.getNumericValue(cprNr.charAt(i));
+			num *= this.cprWeights.get(i);
+			counter += num;
+		}
+		return counter;
+	}
+
+	public boolean isValid() {
+		if (this.number.length() == 10 && this.isNumeric(this.number) && this.date.validDate()
+				&& this.calcWeightCPR(this.number) % 11 == 0)
+			return true;
+		else
+			return false;
 	}
 }
