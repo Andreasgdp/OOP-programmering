0732ad637b2090a520a0f6c7fd2b5fb393afe4b7
Jacob Bregndahl Larsen <45763482+JBLarsen2107@users.noreply.github.com>
2020-10-26 15:27:18 +0100

    Lavede fjernvarme opgave

---
+++ Jarb/Forbruger Distrik Fjernvarme/Forbruger.java
@@ -0,0 +1,45 @@
+public class Forbruger {
+    private String navn;
+    private int maalerNr;
+    private int nyAflaesning;
+    private int forrigeAflaesning;
+    private int maalerMax;
+
+    
+    public Forbruger() {
+        this.navn = "navn";
+        this.maalerNr = 0;
+        this.nyAflaesning = 0;
+        this.forrigeAflaesning = 0;
+        this.maalerMax = 0;
+    }
+
+    public Forbruger(String navn, int maalerNr, int maalerMax) {
+        this.navn = navn;
+        this.maalerNr = maalerNr;
+        this.nyAflaesning = 0;
+        this.forrigeAflaesning = 0;
+        this.maalerMax = maalerMax;
+    }
+
+    public int getMaalerNr() {
+        return maalerNr;
+    }
+
+    public String setNavn(String nytNavn) {
+        return navn = nytNavn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void aflæsMaaler(int forbrugerAflaesning) {
+        forrigeAflaesning = nyAflaesning;
+        nyAflaesning = forbrugerAflaesning;
+    }
+
+    public int beregnForbrug() {
+        return java.lang.Math.abs(maalerMax - (nyAflaesning - forrigeAflaesning));
+    }
+}
