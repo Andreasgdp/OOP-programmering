7641568d5fda9c47102c4082dbba3c48c7b15936
Jacob Bregndahl Larsen <45763482+JBLarsen2107@users.noreply.github.com>
2020-11-02 14:50:05 +0100

    Lavede fjernvarme opg i intelij

---
+++ Jarb/Forbruger Distrik Fjernvarme/main/src/Distrikt.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Distrikt {
+    private String navn;
+    private double prisPrM3;
+    private ArrayList<Forbruger> forbrugere = new ArrayList<Forbruger>();
+    private int antalForbrugere;
+
+    public Distrikt() {
+        this.navn = "navn";
+        this.prisPrM3 = 0;
+        this.antalForbrugere = 0;
+    }
+
+    public Distrikt(String navn, double prisPrM3) {
+        this.navn = navn;
+        this.prisPrM3 = prisPrM3;
+        this.antalForbrugere = 0;
+    }
+
+    public void setForbruger(Forbruger forbruger) {
+        int maxAntalForbrugere = 200;
+        if (antalForbrugere < maxAntalForbrugere) {
+            forbrugere.add(forbruger);
+            antalForbrugere++;
+        } else {
+            System.out.println("Der er ikke plads til flere forbrugere i distriktet!");
+        }
+    }
+
+    public double afregnForbruger(int maalerNr) {
+        double returnValue = -1;
+        for (int i = 0; i < forbrugere.size(); i++) {
+            if (forbrugere.get(i).getMaalerNr() == maalerNr) {
+                Forbruger forbruger = forbrugere.get(i);
+                returnValue = forbruger.beregnForbrug() * prisPrM3;
+                break;
+            }
+        }
+        return returnValue;
+    }
+
+    public String getDistriktNavn() {
+        return navn;
+    }
+
+    public ArrayList<Forbruger> getForbrugerArray() {
+        return forbrugere;
+    }
+}
