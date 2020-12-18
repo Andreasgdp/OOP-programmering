0e303d3d4f592888184d65444984655883664304
JBLarsen2107 <45763482+JBLarsen2107@users.noreply.github.com>
2020-12-18 11:33:00 +0100

    Tilføjede Oles opgaver

---
+++ Jarb/Eksamen/CprNr_Ole/app/src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        CprNr mor = new CprNr("0502201972");
+        String ugedag = mor.getDato().ugeDagTekst();
+        System.out.println("Mor er født på en "+ugedag);
+        CprNr ole = new CprNr("1407532127");
+        ugedag = ole.getDato().ugeDagTekst();
+        System.out.println("Ole er født på en "+ugedag);
+    }
+
+}
